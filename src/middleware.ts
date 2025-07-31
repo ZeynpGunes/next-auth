db009c70a49b323aeb08fc8b1feda2d262066cf0
ZeynpGunes <57145066+ZeynpGunes@users.noreply.github.com>
2025-07-31 22:44:04 +0300

    fix: fixed middleware bug, improved login and dashboard layouts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,9 @@
-// middleware.ts
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
-  console.log("MIDDLEWARE ÇALIŞTI");
   const token = await getToken({ req });
-  console.log(token, "token");
+
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
@@ -14,5 +12,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/dashboard"],
+  matcher: ["/"],
 };
